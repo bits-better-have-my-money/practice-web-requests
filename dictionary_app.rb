544ba9e1a421b870bb579d36496dfa2307431df2
dani <dani.zaghian@gmail.com>
2021-08-20 12:36:18 -0700

    adds bonus to loop continuously

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,24 +1,32 @@
 require "http"
 
-system "clear"
-puts "Welcome to the Dictionary App!"
-puts "Please enter a word:"
-word = gets.chomp
+while true
+  system "clear"
+  puts "Welcome to the Dictionary App!"
+  puts "Please enter a word:"
+  word = gets.chomp
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=YOURAPIKEY")
-definition_data = response.parse(:json)
-if definition_data[0]["text"]
-  p "The definition is: #{definition_data[0]["text"]}"
-else
-  p "The definition is: #{definition_data[1]["text"]}"
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=YOURAPIKEY")
+  definition_data = response.parse(:json)
+  if definition_data[0]["text"]
+    p "The definition is: #{definition_data[0]["text"]}"
+  else
+    p "The definition is: #{definition_data[1]["text"]}"
+  end
+
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=YOURAPIKEY")
+  pronunciation_data = response.parse(:json)
+  p "The pronunciation is: #{pronunciation_data[0]["raw"]}"
+
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/audio?useCanonical=false&limit=50&api_key=YOURAPIKEY")
+  audio_data = response.parse(:json)
+  # p audio_data[0]["fileUrl"]
+  system("open", audio_data[0]["fileUrl"])
+
+  puts "Enter q to quit, any other key to continue"
+    input_option = gets.chomp
+  if input_option == "q"
+    puts "Thank you, goodbye!"
+    break
+  end
 end
-
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=YOURAPIKEY")
-pronunciation_data = response.parse(:json)
-p "The pronunciation is: #{pronunciation_data[0]["raw"]}"
-
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/audio?useCanonical=false&limit=50&api_key=YOURAPIKEY")
-audio_data = response.parse(:json)
-# p audio_data[0]["fileUrl"]
-system("open", audio_data[0]["fileUrl"])
-
